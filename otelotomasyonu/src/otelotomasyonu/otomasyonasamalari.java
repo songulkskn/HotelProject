ba467aafc40673159aadd1705de3bae30bba4836
songulkskn <33230597+songulkskn@users.noreply.github.com>
2019-04-20 20:01:29 +0300

    Add files via upload

---
+++ otelotomasyonu/src/otelotomasyonu/otomasyonasamalari.java
@@ -0,0 +1,9 @@
+bu otomasyonda 4 içerik olacak.
+
+1.Bilgi paneli(Toplam müþteri sayýsý,toplam oda sayýsý,boþ oda sayýsý,dolu oda
+		sayýsý, tek kiþilik oda sayýsý, çift kiþiilik oda sayýsý,oda fiyatlarý
+		(çift kiþilik,tek kiþilik)
+		
+2.Müþteri giriþi(nüfus bilgileri)
+3.Müsteri ayrýlýþý(kalýnan gün sayýsý,ödenecek tutar)
+4.Oda durumu(Seçilen oda durumlarýna göre arama yapar(tek kiþilik,çift kiþilik)).
